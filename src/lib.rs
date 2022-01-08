9baa3757084e960047cd198a0bdcb39246c8fe0c
PonasKovas <mykolas.peteraitis@gmail.com>
2022-01-08 22:27:27 +0200

    entry

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,12 @@
 
 use async_ffi::{FfiFuture, FutureExt};
 use bws_plugin::prelude::*;
-use bws_plugin::register::{PluginEntrySignature, RegPluginStruct};
-
-const ABI_VERSION: u16 = 0;
+use bws_plugin::register::RegPluginStruct;
+use bws_plugin::vtable::BwsVTable;
 
 #[no_mangle]
-static BWS_ABI_VERSION: u64 = ((async_ffi::ABI_VERSION as u64) << 32)
-    | ((bws_plugin::BWS_PLUGIN_ABI_VERSION as u64) << 16)
-    | ABI_VERSION as u64;
+static BWS_ABI_VERSION: u64 =
+    ((async_ffi::ABI_VERSION as u64) << 32) | (bws_plugin::ABI_VERSION as u64);
 
 #[no_mangle]
 unsafe extern "C" fn bws_library_init(register: unsafe extern "C" fn(RegPluginStruct)) {
@@ -19,18 +17,22 @@ unsafe extern "C" fn bws_library_init(register: unsafe extern "C" fn(RegPluginSt
         println!("{}", bt);
     }));
 
-    Plugin::new("test-plugin", (1, 0, 0), entry)
-        .add_dep("anti-grief", "=0.75.0")
-        .register(register);
+    // Plugin::new("test-plugin", (1, 0, 0), entry)
+    //     .add_dep("anti-grief", "=0.75.0")
+    //     .register(register);
 
     Plugin::new("anti-grief", (0, 75, 1), entry).register(register);
 }
 
 extern "C" fn entry(
-    _name: BwsStr,
-    // mut gate: BwsPluginGate,
+    _name: BwsString,
+    vtable: BwsVTable,
+    mut event_receiver: *const (),
     // global_state: BwsGlobalState,
 ) -> FfiFuture<BwsUnit> {
+    // initialize the vtable
+    bws_plugin::vtable::init(vtable);
+
     async move {
         // loop {
         //     match (&mut gate).await {
