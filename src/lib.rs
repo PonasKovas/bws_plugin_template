0bdc40be1a7031f19140e7cdaf616cf6b7c41543
PonasKovas <mykolas.peteraitis@gmail.com>
2022-02-04 18:56:48 +0200

    dep checking

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use abi_stable::std_types::RStr;
+use abi_stable::std_types::{RSlice, RStr, RVec, Tuple2};
 use bws_plugin_interface::BwsPlugin;
 
 #[no_mangle]
@@ -8,4 +8,5 @@ static BWS_ABI: u32 = bws_plugin_interface::ABI;
 static BWS_PLUGIN_ROOT: BwsPlugin = BwsPlugin {
     name: RStr::from_str("plugin_template"),
     version: RStr::from_str(env!("CARGO_PKG_VERSION")),
+    dependencies: RSlice::from_slice(&[]),
 };
