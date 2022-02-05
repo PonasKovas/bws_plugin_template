34be4cd8110651210979c2ecc3552b90c5c2ab48
PonasKovas <mykolas.peteraitis@gmail.com>
2022-02-05 18:45:39 +0200

    new stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,26 @@
 use abi_stable::std_types::{RSlice, RStr, RVec, Tuple2};
-use bws_plugin_interface::{global_state::GState, BwsPlugin};
+use bws_plugin_interface::{
+    debug, error,
+    global_state::{vtable::LogLevel, GState},
+    info, trace, warn, BwsPlugin,
+};
 
 #[no_mangle]
 static BWS_ABI: u32 = bws_plugin_interface::ABI;
 
 #[no_mangle]
 static BWS_PLUGIN_ROOT: BwsPlugin = BwsPlugin {
-    name: RStr::from_str("plugin_template"),
+    name: RStr::from_str(env!("CARGO_PKG_NAME")),
     version: RStr::from_str(env!("CARGO_PKG_VERSION")),
-    dependencies: RSlice::from_slice(&[Tuple2(RStr::from_str("plugin1"), RStr::from_str("*"))]),
+    dependencies: RSlice::from_slice(&[]),
     enable,
     disable,
 };
 
 fn enable(gstate: &GState) {
-    println!("Plugin template enabled");
-    gstate
-        .read()
-        .plugins
-        .iter()
-        .find(|p| p.name() == "plugin1")
-        .expect("plugin1 not found")
-        .disable(gstate)
-        .expect("plugin1 already disabled?");
+    info!(@gstate, "Started");
 }
-fn disable(_gstate: &GState) {
-    println!("Plugin template disabled");
+fn disable(gstate: &GState) {
+    error!(@gstate, "Stopped");
+    info!(@gstate, "Stopped");
 }
